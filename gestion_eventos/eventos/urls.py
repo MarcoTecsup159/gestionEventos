e022303e3ad01b48beedf50154e6fddea7e0246d
MarcoTecsup159 <168231703+MarcoTecsup159@users.noreply.github.com>
2024-09-28 21:58:44 -0500

    Aplicacion funcionando con funciones basicas

---
+++ gestion_eventos/eventos/urls.py
@@ -0,0 +1,10 @@
+
+from django.urls import path
+from .views import CrearEventoView, registrar_en_evento, lista_eventos, detalle_evento
+
+urlpatterns = [
+    path('crear/', CrearEventoView.as_view(), name='crear_evento'),  # Ruta para crear un nuevo evento
+    path('registrar/<int:evento_id>/', registrar_en_evento, name='registrar_evento'),  # Ruta para registrar usuario en un evento
+    path('', lista_eventos, name='listar_eventos'),
+    path('<int:evento_id>/', detalle_evento, name='detalle_evento'),
+]
--- gestion_eventos/eventos/views.py
+++ gestion_eventos/eventos/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from django.views.generic import CreateView, ListView
+from django.urls import reverse_lazy
+from .models import Evento, RegistroEvento
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+#crear eventos
+class CrearEventoView(CreateView):
+    model = Evento
+    fields = ['nombre', 'descripcion', 'fecha', 'lugar', 'organizador']
+    template_name = 'eventos/crear_evento.html'
+    success_url = reverse_lazy('listar_eventos')
+
+# registrar usuario en un evento
+@login_required
+def registrar_en_evento(request, evento_id):
+    evento = get_object_or_404(Evento, id=evento_id)
+    RegistroEvento.objects.get_or_create(usuario=request.user, evento=evento)
+    return redirect('detalle_evento', evento_id=evento_id)
+
+def lista_eventos(request):
+    eventos = Evento.objects.all()
+    return render(request, 'eventos/lista_eventos.html', {'eventos': eventos})
+
+def detalle_evento(request, evento_id):
+    evento = get_object_or_404(Evento, id=evento_id)
+    return render(request, 'eventos/detalle_evento.html', {'evento': evento})
